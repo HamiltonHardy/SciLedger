f9e8966663350784aa5946d5fb3c78ecadc44cb9
Re8Hoo <re8hoopes@aggiemail.usu.edu>
2022-07-22 12:09:59 -0600

    Added the workflow/merkle tree stuff into the vault code

---
+++ BREU_SourceCode/Edits - VAULT - Copy/src/workflowGen/randomizeGen.java
@@ -0,0 +1,34 @@
+package workflowGen;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Random;
+import java.util.*;
+
+public class randomizeGen {
+    static int maxWorkflows =10;
+
+    static String startPoint;
+    static ArrayList<workflow> workflows = new ArrayList<>();
+
+    public static void generate(){
+        workflows.add(new workflow(0,null,null));
+        Random rand = new Random();
+        for(int i=1; i<maxWorkflows; i++) {
+            workflow w = new workflow(i,startPoint,String.valueOf(rand.nextInt(i)));
+            workflows.add(w);
+            startPoint = w.forNextWf.getTaskID();
+        }
+    }
+
+    public static void main(String[] args) {
+        generate();
+        System.out.println(workflows);
+
+
+    }
+
+
+}
